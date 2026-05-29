33d80936a40b9b1d83c4e16a01c4594b82df655b
duduselmer <edu.selmer@gmail.com>
2026-05-29 00:09:56 -0300

    Update usuarioRoutes.js

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const router = express.Router();
 const UsuarioService = require('../services/usuarioService');
 const { autenticar, autorizar } = require('../middleware/auth');
